cbda21214173735572993e180709c0353690088f
Tunaberkyrky <tunaberk.software@outlook.com>
2025-04-05 12:06:20 +0300

    for-rent page and function

--- main.js
+++ main.js
@@ -90,6 +90,98 @@ const listings = [
         "portfolios/for-sale/Köy 3+1 Bahçe Dubleks/DJI_20221229_164524_458.jpg",
         
     ]
+    },
+    {
+      id: 5,
+      title: "KÖY Live and Work",
+      description: "Köy projesinin en gözde etaplarından, yaşam alanları ile ofis ortamlarını beraber sunan Live and Work etabında modern tasarımlı balkonlu 1+1 daire.",
+      price: 50000,
+      location: "İstanbul, Sarıyer/Zekeriyaköy",
+      area: 70,
+      room_count: "1+1",
+      bathroom_count: 1,
+      category: "Kiralık",
+      created_at: "2025-03-30T10:00:00Z",
+      images: [
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_1511.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_1512.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_1513.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_1514.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_1515.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_1518.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_1519.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_1520.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_4424.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_4427.jpeg",
+        "portfolios/for-sale/KÖY 1+1 Live and Work/IMG_4430.jpeg"
+      ]
+    },
+    {
+      id: 6,
+      title: "Garanti Koza",
+      description: "Doğayla iç içe bir yaşam için doğru adres olan Zekeriyaköy'deki bu evin 905 m2 bahçe içerisinde; otomatik sulama sistemi, jeneratörü ve su kuyusu bulunmaktadır.",
+      price: 120000,
+      location: "İstanbul, Sarıyer/Zekeriyaköy",
+      area: 485,
+      room_count: "6+2",
+      bathroom_count: "4",
+      category: "Kiralık",
+      created_at: "2025-03-29T12:00:00Z",
+      images: [
+       "portfolios/for-sale/Garanti Koza/IMG_0627.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_0628.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_0630.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_0635.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_0639.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_0643.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_0649.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_0653.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_0656.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_0662.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_1254.JPG",
+       "portfolios/for-sale/Garanti Koza/IMG_1257.JPG"
+      ]
+    },
+    {
+      id: 7,
+      title: "KÖY 3C Villa",
+      description: "Kapsamlı yaşam olanakları sunan müstakil villamız, KÖY sitesinin zengin sosyal olanakları ile kusursuz bir yaşam alanı sunuyor.",
+      price: 95000,
+      location: "İstanbul, Sarıyer/Zekeriyaköy",
+      area: 338,
+      room_count: "5+2",
+      bathroom_count: "4",
+      category: "Kiralık",
+      created_at: "2025-03-28T09:30:00Z",
+      images: [
+        "portfolios/for-sale/KÖY 3- C VİLLA 5+2/IMG_2896.jpg",
+        "portfolios/for-sale/KÖY 3- C VİLLA 5+2/IMG_2897.jpg",
+        "portfolios/for-sale/KÖY 3- C VİLLA 5+2/IMG_2898.jpg",
+        "portfolios/for-sale/KÖY 3- C VİLLA 5+2/IMG_2899.jpg",
+        "portfolios/for-sale/KÖY 3- C VİLLA 5+2/IMG_2900.jpg",
+        "portfolios/for-sale/KÖY 3- C VİLLA 5+2/IMG_2901.jpg",
+        "portfolios/for-sale/KÖY 3- C VİLLA 5+2/IMG_2908.jpg",
+        "portfolios/for-sale/KÖY 3- C VİLLA 5+2/IMG_2913.jpg",
+        "portfolios/for-sale/KÖY 3- C VİLLA 5+2/IMG_2928.jpg"
+    ]
+    },
+    {
+      id: 8,
+      title: "KÖY Bahçe Dubleks",
+      description: "Kapsamlı yaşam olanakları sunan müstakil villamız, KÖY sitesinin zengin sosyal olanakları ile kusursuz bir yaşam alanı sunuyor.",
+      price: 90000,
+      location: "İstanbul, Sarıyer/Zekeriyaköy",
+      area: 338,
+      room_count: "5+2",
+      bathroom_count: "4",
+      category: "Kiralık",
+      created_at: "2025-03-28T09:30:00Z",
+      images: [
+        "portfolios/for-sale/Köy 3+1 Bahçe Dubleks/DJI_20221229_164446_419.jpg",
+        "portfolios/for-sale/Köy 3+1 Bahçe Dubleks/DJI_20221229_164511_169.jpg",
+        "portfolios/for-sale/Köy 3+1 Bahçe Dubleks/DJI_20221229_164524_458.jpg",
+        
+    ]
     }
   ];
 
@@ -98,7 +190,8 @@ const listings = [
 
 
   // Select the parent container
-let contentContainer = document.querySelector(".content-container");
+let forSaleContentContainer = document.querySelector(".for-sale-content-container");
+let forRentContentContainer = document.querySelector(".for-rent-content-container");
 
 // Create a new content-item-card
 function createContentItemCard(listing) {
@@ -189,11 +282,30 @@ function createContentItemCard(listing) {
 
 console.log("hello world")
 
-for (let i = 0; i < listings.length; i++) {
-    let newCard = createContentItemCard(listings[i]);
-    contentContainer.appendChild(newCard);
+
+
+if (forSaleContentContainer) {
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].category === "Satılık") {
+      let newCard = createContentItemCard(listings[i]);
+      forSaleContentContainer.appendChild(newCard);
+    }
+  }
+} else {
+  console.error("forSaleContentContainer not found in the DOM.");
 }
 
+
+if (forRentContentContainer) {
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].category === "Kiralık") {
+      let newCard = createContentItemCard(listings[i]);
+      forRentContentContainer.appendChild(newCard);
+    }
+  }
+} else {
+  console.error("forRentContentContainer not found in the DOM.");
+}
 
 
 
